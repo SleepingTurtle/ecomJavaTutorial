c90b84a174152ce75794824f35b18842fb65c61f
Josh <joshcharlestechie@gmail.com>
2023-10-07 10:29:37 -0400

    init commit
    Working my way through

---
+++ src/main/java/com/educative/ecommerce/service/CategoryService.java
@@ -0,0 +1,40 @@
+package com.educative.ecommerce.service;
+
+import com.educative.ecommerce.model.Category;
+import com.educative.ecommerce.repository.Categoryrepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private Categoryrepository categoryrepository;
+
+    public List<Category> listCategory() {
+        return categoryrepository.findAll();
+    }
+
+    public void createCategory(Category category) {
+        categoryrepository.save(category);
+    }
+
+    public Category readCategory(String categoryName) {
+        return categoryrepository.findByCategoryName(categoryName);
+    }
+
+    public Optional<Category> readCategory(Integer categoryId) {
+        return categoryrepository.findById(categoryId);
+    }
+
+    public void updateCategory(Integer categoryId, Category newCategory) {
+        Category category = categoryrepository.findById(categoryId).get();
+        category.setCategoryName(newCategory.getCategoryName());
+        category.setDescription(newCategory.getDescription());
+        category.setImageUrl(newCategory.getImageUrl());
+        categoryrepository.save(category);
+    }
+}
